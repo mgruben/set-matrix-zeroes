3ea874c554408784fdedfa58fb75780844990553
mgruben <GrubenM@gmail.com>
2016-09-20 17:00:37 -0500

    Implement vector-based zeroing solution

--- SetMatrixZeroes.cpp
+++ SetMatrixZeroes.cpp
@@ -7,16 +7,30 @@ using namespace std;
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        vector<int> rowsToZero;
+        vector<int> colsToZero;
+        
+        // Scan the array for zeros
         for (int i = 0; i < matrix.size(); i++) {
             for (int j = 0; j < matrix[0].size(); j++) {
                 if (matrix[i][j] == 0) {
-                    for (int k = 0; k < matrix.size(); k++) {
-                        matrix[k][j] = 0;
-                    }
-                    for (int k = 0; k < matrix[0].size(); k++) {
-                        matrix[i][k] = 0;
-                    }
+                    rowsToZero.push_back(i);
+                    colsToZero.push_back(j);
                 }
+            }
+        }
+        
+        // Zero the rows
+        for (int i = 0; i < rowsToZero.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                matrix[rowsToZero[i]][j] = 0;
+            }
+        }
+        
+        // Zero the columns
+        for (int j = 0; j < colsToZero.size(); j++) {
+            for (int i = 0; i < matrix.size(); i++) {
+                matrix[i][colsToZero[j]] = 0;
             }
         }
     }
