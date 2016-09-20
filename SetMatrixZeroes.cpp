1f6f068c9a783e631b7e50d93bb13d85c8dcc285
mgruben <GrubenM@gmail.com>
2016-09-20 16:39:50 -0500

    Iterate through matrix

--- SetMatrixZeroes.cpp
+++ SetMatrixZeroes.cpp
@@ -6,10 +6,18 @@ using namespace std;
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                cout << matrix[i][j];
+            }
+            cout << endl;
+        }
     }
 };
 
 int main() {
+    vector<vector<int>> m = {{1,2,3},{4,5,6},{7,8,9}};
+    Solution sol;
+    sol.setZeroes(m);
     return 0;
 }
